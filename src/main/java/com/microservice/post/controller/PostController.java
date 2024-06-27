f99eec06dfafc28885ba9d83a38be34766d098c9
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-27 15:34:44 +0530

    .

--- src/main/java/com/microservice/post/controller/PostController.java
+++ src/main/java/com/microservice/post/controller/PostController.java
@@ -1,4 +1,21 @@
 package com.microservice.post.controller;
 
+import com.microservice.post.entity.Post;
+import com.microservice.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+
+    @PostMapping
+    public ResponseEntity<Post> savePost(@RequestBody Post post) {
+        return null;
+    }
 }
