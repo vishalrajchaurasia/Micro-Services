a8e9f21a57561fe7fbe30964a3c049c57c01cc7f
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-27 15:47:50 +0530

    .

--- src/main/java/com/microservice/post/controller/PostController.java
+++ src/main/java/com/microservice/post/controller/PostController.java
@@ -6,10 +6,7 @@ import com.microservice.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -21,5 +18,10 @@ public class PostController {
     public ResponseEntity<Post> savePost(@RequestBody Post post) {
         Post newPost = postService.savePost(post);
         return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
+    @GetMapping("/{postId}")
+    public Post getPostByPostId(@PathVariable String postId){//to find the post based on post id
+        Post post = postService.findPostById(postId);
+        return post;
     }
 }
