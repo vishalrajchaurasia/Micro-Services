838d77c90e08606931376c2fc0460025f9bfc88e
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-27 15:51:58 +0530

    .

--- src/main/java/com/microservice/post/service/PostService.java
+++ src/main/java/com/microservice/post/service/PostService.java
@@ -5,6 +5,7 @@ import com.microservice.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -13,9 +14,14 @@ public class PostService {
     private PostRepository postRepository;
 
     public Post savePost(Post post){
-        String postId = UUID.randomUUID().toString();
+        String postId=UUID.randomUUID().toString();
         post.setId(postId);
         Post savedPost=postRepository.save(post);
         return savedPost;
+    }
+
+    public Post findPostById(String postId) {
+        Post post = postRepository.findById(postId).get();
+        return post;
     }
 }
