f197ae87f935e234bf579693ed7b3a170d0700f8
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-28 22:28:00 +0530

    some change in Post.java

--- src/main/java/com/microservice/post/entity/Post.java
+++ src/main/java/com/microservice/post/entity/Post.java
@@ -1,8 +1,7 @@
 package com.microservice.post.entity;
 
-import jakarta.persistence.*;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
