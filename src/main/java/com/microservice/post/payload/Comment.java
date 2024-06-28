919971725e16d38dac7af9a0e678cd24f7cd3fab
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-28 12:55:23 +0530

    some change in RestTemplateConfig.java

---
+++ src/main/java/com/microservice/post/payload/Comment.java
@@ -0,0 +1,17 @@
+package com.microservice.post.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {//when you call that api i get the comment
+    private String commentId;
+    private String name;
+    private String email;
+    private String body;
+    private String postId;
+
+}
