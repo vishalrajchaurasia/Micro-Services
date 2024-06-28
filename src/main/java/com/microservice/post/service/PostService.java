4593c73eb284ac018c86cca9cadd7ae0cf04b1f1
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-28 15:25:43 +0530

    some change in PostService.java

--- src/main/java/com/microservice/post/service/PostService.java
+++ src/main/java/com/microservice/post/service/PostService.java
@@ -1,11 +1,14 @@
 package com.microservice.post.service;
 
+import com.microservice.post.config.RestTemplateConfig;
 import com.microservice.post.entity.Post;
 import com.microservice.post.payload.PostDto;
 import com.microservice.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,6 +16,8 @@ import java.util.UUID;
 public class PostService {
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private RestTemplateConfig restTemplate;
 
     public Post savePost(Post post){
         String postId=UUID.randomUUID().toString();
@@ -27,6 +32,14 @@ public class PostService {
     }
 
     public PostDto getPostWithComments(String postId) {
-
+        Post post = postRepository.findById(postId).get();//this will directly give me post object
+        ArrayList comments = restTemplate.getRestTemplate().getForObject("http://localhost:8082/api/comments/" + postId, ArrayList.class);//it is return back is list means ArrayList.class
+        PostDto postDto = new PostDto();
+        postDto.setPostId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        postDto.setComments(comments);
+        return postDto;
     }
 }
