80ddfe68224aa7d85cf10e604b2ccea93f79ad5d
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-28 12:56:33 +0530

    some change in PostDto.java

---
+++ src/main/java/com/microservice/post/payload/PostDto.java
@@ -0,0 +1,21 @@
+package com.microservice.post.payload;
+
+import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.stream.events.Comment;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDto {
+    //what you want to return back
+    //return back to postDetails
+    private String postId;
+    private String title;
+    private String description;
+    private String content;
+    List<Comment> comments;//here return back list of comments
+}
