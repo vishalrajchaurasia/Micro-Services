bcb0d734d8a2640b1a6e54c9261eea924c471aa1
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-30 23:18:34 +0530

    .

--- src/main/java/com/microservice/post/repository/PostRepository.java
+++ src/main/java/com/microservice/post/repository/PostRepository.java
@@ -3,5 +3,5 @@ package com.microservice.post.repository;
 import com.microservice.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,String> {//
+public interface PostRepository extends JpaRepository<Post,String> {
 }
