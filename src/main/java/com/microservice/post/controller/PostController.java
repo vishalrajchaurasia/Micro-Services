17ad63d06b85b29daa828608a937fd7df7ff762d
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-07-01 16:13:28 +0530

    .

--- src/main/java/com/microservice/post/controller/PostController.java
+++ src/main/java/com/microservice/post/controller/PostController.java
@@ -5,6 +5,7 @@ import com.microservice.post.payload.PostDto;
 import com.microservice.post.repository.PostRepository;
 import com.microservice.post.service.PostService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.ws.rs.POST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,5 +35,16 @@ public class PostController {
        PostDto postDto = postService.getPostWithComments(postId);
        return new ResponseEntity<>(postDto,HttpStatus.OK);
     }
+    public ResponseEntity<PostDto> commentFallback(String postId,Exception ex){
+        System.out.println("Fallback is executed because service is down : "+ ex.getMessage());
 
+        ex.printStackTrace();
+        PostDto dto = new PostDto();
+        dto.setId("1234");
+        dto.setTitle("Service Down");
+        dto.setContent("Service Down");
+        dto.setDescription("Service Down");
+        //dto.setComment("Service Down");
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
 }
