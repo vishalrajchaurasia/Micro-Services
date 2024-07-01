285136e128972e67b8c1ae3f3eb8e2282571897a
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-07-01 16:03:18 +0530

    .

--- src/main/java/com/microservice/post/controller/PostController.java
+++ src/main/java/com/microservice/post/controller/PostController.java
@@ -4,6 +4,7 @@ import com.microservice.post.entity.Post;
 import com.microservice.post.payload.PostDto;
 import com.microservice.post.repository.PostRepository;
 import com.microservice.post.service.PostService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,8 +29,10 @@ public class PostController {
     }
     //http://localhost:8081/api/posts/{postId}/comments
     @GetMapping("/{postId}/comments")
+    @CircuitBreaker(name= "commentBreaker", fallbackMethod = "commentFallback")//what is fallBack if the service is down then it is call back method commentFallback automatically
     public ResponseEntity<PostDto> getPostWithComments(@PathVariable String postId){
        PostDto postDto = postService.getPostWithComments(postId);
        return new ResponseEntity<>(postDto,HttpStatus.OK);
     }
+
 }
